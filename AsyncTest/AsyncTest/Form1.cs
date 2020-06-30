1dd48b4e6c4d8a270f30f36038fd8479020538d0
kaushalyansbm <66802313+kaushalyansbm@users.noreply.github.com>
2020-06-30 14:01:26 +0530

    Add files via upload
    test

---
+++ AsyncTest/AsyncTest/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int StringCounter() {
+            int count = 0;
+           
+
+               
+                using (StreamReader SM = new StreamReader("C:\\Users\\Kaushalya.h\\source\\repos\\AsyncTest\\AsyncTest\\x.txt"))
+                {
+
+                    
+                    string counter = SM.ReadToEnd();
+                    count = counter.Length;
+                    Thread.Sleep(7000);
+
+
+
+
+                }
+           
+            return count;
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+          Task<int> x = new Task<int>(StringCounter);
+            x.Start();
+            label1.Text = "still processing";
+            int count =await x;
+            label1.Text = count.ToString() + "numberof characters";
+
+        }
+    }
+}
